f3bbf13701ee0a282f43905f82fd885d1add3ce8
Eliseu Martinho <eliseu.martinho@gmail.com>
2015-12-31 19:37:17 +0000

    removed not needed folder

--- image/config/30_presetup_nginx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [[ "$PASSENGER_APP_ENV" != "" ]]; then
-	echo "passenger_app_env '$PASSENGER_APP_ENV';" > /etc/nginx/conf.d/00_app_env.conf
-fi
--- image/nodejs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-source /pd_build/buildconfig
-set -x
-
-## Install Node.js (also needed for Rails asset compilation)
-apt-get install -y nodejs
