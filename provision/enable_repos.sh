7532f84683af7dc13ff18e5114c84a93062327a0
Eliseu Martinho <eliseu.martinho@gmail.com>
2016-02-15 10:21:30 +0000

    Mongo version bump to 3.2

--- provision/enable_repos.sh
+++ provision/enable_repos.sh
@@ -10,17 +10,20 @@ echo deb https://oss-binaries.phusionpassenger.com/apt/passenger trusty main > /
 echo deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu trusty main > /etc/apt/sources.list.d/openjdk8.list
 
 ## Mongo DB 3.0 PPA
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+#echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
 
 # The recv-keys part takes a bit of time, so it's faster to receive multiple keys at once.
 #
 # Phusion Passenger
 # OpenJDK 8 PPA
 # Mongo
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys \
 	561F9B9CAC40B2F7 \
 	DA1A4A13543B466853BAF164EB9B1D8886F44E2A \
-	7F0CEB10
+	EA312927
 
 #apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
